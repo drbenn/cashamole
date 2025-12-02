658a59c23332dddae33ea4d4d8c9357c7856dba5
dan bennett <bennett.daniel@gmail.com>
2025-12-01 21:08:56 -0500

    shadcn working in desktop

--- desktop/src/App.vue
+++ desktop/src/App.vue
@@ -1,4 +1,6 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Button } from '@/components/ui/button'
+</script>
 
 <template>
   <h1>You did it!</h1>
@@ -6,16 +8,8 @@
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
   </p>
-</template>
 
-<style scoped></style>
-
-<!-- <script setup lang="ts">
-import { Button } from '@/components/ui/button'
-</script>
-
-<template>
-  <div>
+    <div>
     <div class="text-cyan-700 font-black text-8xl">
       YO
     </div>
@@ -23,6 +17,9 @@ import { Button } from '@/components/ui/button'
       <Button>Click me</Button>
     </div>
   </div>
+
 </template>
 
-<style scoped></style> -->
+<style scoped></style>
+
+
