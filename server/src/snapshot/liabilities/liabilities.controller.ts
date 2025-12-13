25fe709a353985ceba9f0760b49a617a35f07c2d
dan bennett <bennett.daniel@gmail.com>
2025-12-13 00:21:07 -0500

    basic asset and liabilties controllers and services

---
+++ server/src/snapshot/liabilities/liabilities.controller.ts
@@ -0,0 +1,4 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('snapshot/liabilities')
+export class LiabilitiesController {}
--- server/src/snapshot/snapshot.module.ts
+++ server/src/snapshot/snapshot.module.ts
@@ -4,13 +4,30 @@ import { SnapshotService } from './snapshot.service';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from 'src/database/database.module';
 import { SnapshotQueryService } from './snapshot-query.service';
+import { AssetsController } from './assets/assets.controller';
+import { LiabilitiesController } from './liabilities/liabilities.controller';
+import { AssetsService } from './assets/assets.service';
+import { AssetQueryService } from './assets/assets-query.service';
+import { LiabilitiesService } from './liabilities/liabilities.service';
+import { LiabilitiesQueryService } from './liabilities/liabilties-query.service';
 
 @Module({
   imports: [
     ConfigModule,
     DatabaseModule
   ],
-  controllers: [SnapshotController],
-  providers: [SnapshotService, SnapshotQueryService]
+  controllers: [
+    SnapshotController,
+    AssetsController,
+    LiabilitiesController
+  ],
+  providers: [
+    SnapshotService,
+    SnapshotQueryService,
+    AssetsService,
+    AssetQueryService,
+    LiabilitiesService,
+    LiabilitiesQueryService
+  ]
 })
 export class SnapshotModule {}
